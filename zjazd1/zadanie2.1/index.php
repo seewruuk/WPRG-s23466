4103285e66995c8bf92dd015cbcf06b82d45de35
seewruuk <sewruczekk@icloud.com>
2023-03-24 12:47:09 +0100

    zadanie 2.1

---
+++ zjazd1/zadanie2.1/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$array=[];
+
+for($i = 0; $i<10; $i++){
+    $array[$i] = rand(1,100);
+}
+for($i = 0; $i<10; $i++){
+    echo $array[$i]." ";
+}
+echo "\n";
+
+
+
+
+do{
+    echo "Podaj wartosc od 0 do 10"."\n";
+    $szukana_wartosc = readline("Podaj wartosc: ");
+}while($szukana_wartosc < 0 || $szukana_wartosc >= 10);
+
+echo "Szukana wartosc: ".$array[$szukana_wartosc];
+
+
+// echo rand(5, 15);
+
+?>
